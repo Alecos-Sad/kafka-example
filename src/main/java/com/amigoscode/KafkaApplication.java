988f489d9d03dccca9f23f9bc43814a0e4143047
Sadonikov Alexander <alecos100@gmail.com>
2023-06-13 21:00:51 +0300

    5. Написание конфигурации consumer

--- src/main/java/com/amigoscode/KafkaApplication.java
+++ src/main/java/com/amigoscode/KafkaApplication.java
@@ -19,7 +19,7 @@ public class KafkaApplication {
     @Bean
     CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate){
         return args -> {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 100; i++) {
                 kafkaTemplate.send("amigoscode", "cool " + i);
             }
         };
