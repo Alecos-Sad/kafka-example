fce75144453736c47f72682de977aadb5d7d879d
Sadonikov Alexander <alecos100@gmail.com>
2023-06-13 21:18:44 +0300

    7. Написание REST controller

--- src/main/java/com/amigoscode/KafkaListeners.java
+++ src/main/java/com/amigoscode/KafkaListeners.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 public class KafkaListeners {
 
     @KafkaListener(topics = "amigoscode", groupId = "groupId")
-    void listener(String data){
+    void listener(String data) {
         System.out.println("Listener received: " + data + "  ))");
     }
 }
